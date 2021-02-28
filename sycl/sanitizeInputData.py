2de89633d3da19ee36257ea03cde83d138dd13fd
Mojan Jamalzadeh <mojanjamalzadeh@yahoo.com>
2021-02-28 10:29:26 -0800

    add sanitization for aif and vif files

--- sycl/sanitizeInputData.py
+++ sycl/sanitizeInputData.py
@@ -2,14 +2,29 @@ import numpy as np #importing numpy module for efficiently executing numerical o
 import matplotlib.pyplot as plt #importing the pyplot from the matplotlib library
 from scipy import signal
 
-with open('data/c_noise.txt') as f:
+with open('data/c_real.txt') as f:
     lines = f.readlines()
     c_t = [float(line.split()[0]) for line in lines]
     c_i = [float(line.split()[2]) for line in lines]
 
+with open('data/aif_real.txt') as f:
+    lines = f.readlines()
+    aif_t = [float(line.split()[0]) for line in lines]
+    aif_i = [float(line.split()[2]) for line in lines]
+
+with open('data/vif_real.txt') as f:
+    lines = f.readlines()
+    vif_t = [float(line.split()[0]) for line in lines]
+    vif_i = [float(line.split()[2]) for line in lines]
 # - - - # We load the data in the mat format but this code will work for any sort of time series.# - - - #
 cT = np.array(c_t)
 cI = np.array(c_i)
+
+aifT = np.array(aif_t)
+aifI = np.array(aif_i)
+
+vifT = np.array(vif_t)
+vifI = np.array(vif_i)
 
 # Visualizing the original and the Filtered Time Series
 fig = plt.figure()
@@ -27,6 +42,15 @@ cIfilt = signal.filtfilt(b, a, cI)
 cIfilt=np.array(cIfilt)
 cIfilt=cIfilt.transpose()
 
+aifIfilt = signal.filtfilt(b, a, aifI)
+aifIfilt=np.array(aifIfilt)
+aifIfilt=aifIfilt.transpose()
+
+vifIfilt = signal.filtfilt(b, a, vifI)
+vifIfilt=np.array(vifIfilt)
+vifIfilt=vifIfilt.transpose()
+
+
 # write filtered data back into a txt file
 zeroArr = np.zeros((cT.shape[0],), dtype=int)
 data = np.column_stack((cT,zeroArr, cIfilt, zeroArr))
@@ -34,6 +58,23 @@ with open('data/sanitized_c.txt', 'w') as txt_file:
     for row in data:
         line = ' '.join(str(v) for v in row)
         txt_file.write(line + "\n") # works with any number of elements in a line
+
+# write filtered data back into a txt file
+zeroArr = np.zeros((cT.shape[0],), dtype=int)
+data = np.column_stack((aifT,zeroArr, aifIfilt, zeroArr))
+with open('data/sanitized_aif.txt', 'w') as txt_file:
+    for row in data:
+        line = ' '.join(str(v) for v in row)
+        txt_file.write(line + "\n") # works with any number of elements in a line
+
+# write filtered data back into a txt file
+zeroArr = np.zeros((cT.shape[0],), dtype=int)
+data = np.column_stack((vifT,zeroArr, vifIfilt, zeroArr))
+with open('data/sanitized_vif.txt', 'w') as txt_file:
+    for row in data:
+        line = ' '.join(str(v) for v in row)
+        txt_file.write(line + "\n") # works with any number of elements in a line
+
 
 ax.plot(cT, cIfilt, 'b', linewidth=1)
 ax.set_xlabel('Time (s)',fontsize=18)
