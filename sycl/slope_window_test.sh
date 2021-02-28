a58284c4fbda1ba67f7e13fde023639f928423d8
Mojan Jamalzadeh <mojanjamalzadeh@yahoo.com>
2021-02-28 12:31:50 -0800

    add slope_window as a global variable to be set by run_model for testing purposes, script to run the model with various slope window values

---
+++ sycl/slope_window_test.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+slope_window_set="5 10 15 20 50"
+for slope_window in $slope_window_set; do
+    echo "slope_window: $slope_window"
+    run_model -a data/aif.txt -v data/vif.txt -c data/c.txt -w $slope_window >> slope_window_exp.txt
+done
