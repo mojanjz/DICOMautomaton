653f4fa2e836c4a77b1e5c9a575ed03413adcd60
tiannamh <tianna_fella@hotmail.com>
2021-03-04 17:23:04 -0800

    Added visualization for effect of slope window on K ratios. User must now input, in order: k1A k1V k2 with one space in between when running the python file.

--- sycl/slopeWindowVisualization.py
+++ sycl/slopeWindowVisualization.py
@@ -7,9 +7,16 @@ import sys
 #This intakes a file called slope_window_exp.txt
 #The file is in the form of:
 #SlopeWindow Slope Intercept TimeMidpoint K1A K1V K2
-# k1AExpected = sys.argv[1]
-# k1VExpected = sys.argv[2]
-# k2Expected = sys.argv[3]
+try:
+    k1AExpected = float(sys.argv[1])
+    k1VExpected = float(sys.argv[2])
+    k2Expected = float(sys.argv[3])
+except IndexError:
+   print("Please input k1A k1V k2 Expected values")
+   sys.exit(1)
+
+k1AExpectedRatio = k1AExpected/(k1AExpected+k1VExpected)
+k1VExpectedRatio = k1VExpected/(k1AExpected+k1VExpected)
 
 #The c file input to the model
 with open('c.txt') as f: #grab expected data
@@ -29,10 +36,17 @@ with open('slope_window_exp.txt') as f: #grab expected data
 
 numberExperiments = len(slopeWindow) #the number of experiments in this text file
 
-# k1AExpected = [k1AExpected]*numberExperiments
+#Force the values into lists to be able to plot
+k1AExpected = [k1AExpected]*numberExperiments
+k1VExpected = [k1VExpected]*numberExperiments
+k2Expected = [k2Expected]*numberExperiments
+k1AExpectedRatio = [k1AExpectedRatio]*numberExperiments
+k1VExpectedRatio = [k1VExpectedRatio]*numberExperiments
 
 slopeCVals = []
 slopetVals = []
+k1ARatios = []
+k1VRatios = []
 
 for i in range (numberExperiments):
     slopeCValsIntermediate = []
@@ -43,6 +57,8 @@ for i in range (numberExperiments):
         tVal = tVal+1
     slopeCVals.append(slopeCValsIntermediate)
     slopetVals.append(slopetValsIntermediate)
+    k1ARatios.append(k1A[i]/(k1A[i]+k1V[i]))
+    k1VRatios.append(k1V[i]/(k1A[i]+k1V[i]))
 
 plt.xlabel("Time")
 plt.ylabel("Contrast Agent Intensity")
@@ -54,33 +70,50 @@ plt.legend()
 plt.plot(cET,cEVal)
 # plt.show()
 
-plt.subplot(1, 4, 1)
+plt.subplot(2, 3, 1)
 plt.plot(slopeWindow,k1A,'bo')
-# plt.plot(slopeWindow, k1AExpected, 'r--', label = 'Expected k1A Value')
+plt.plot(slopeWindow, k1AExpected, 'b--')
 plt.title("Slope Window vs K1A")
 plt.xlabel("Slope Window")
 plt.ylabel("k1A Value")
 
-plt.subplot(1, 4, 2)
+plt.subplot(2, 3, 2)
 plt.plot(slopeWindow,k1V, 'ro')
+plt.plot(slopeWindow, k1VExpected, 'r--')
 plt.title("Slope Window vs K1V")
 plt.xlabel("Slope Window")
 plt.ylabel("k1V Value")
 
-plt.subplot(1, 4, 3)
+plt.subplot(2, 3, 3)
 plt.plot(slopeWindow,k2, 'go')
+plt.plot(slopeWindow, k2Expected, 'g--')
 plt.title("Slope Window vs K2")
 plt.xlabel("Slope Window")
 plt.ylabel("k2 Value")
 
-plt.subplot(1, 4, 4)
+plt.subplot(2, 3, 4)
 for i in range(len(slopeCVals)):
-    # plt.plot(slopetVals,[pt[i] for pt in slopeCVals],label = 'id %s'%i)
     plt.plot(slopetVals[i],slopeCVals[i], label = 'Slope Window %s'%slopeWindow[i])
 plt.legend()
 plt.plot(cET,cEVal)
 plt.xlabel("Time")
 plt.ylabel("Contrast Agent Intensity")
 plt.title("The effect of Slope Window Size on Slope")
+
+plt.subplot(2, 3, 5)
+plt.plot(slopeWindow,k1AExpectedRatio, 'g--', label = 'Expected Ratio')
+plt.plot(slopeWindow, k1ARatios, 'b', label = 'Actual Ratio')
+plt.legend()
+plt.title("The Effect of Slope Window on K1A Ratio")
+plt.xlabel("Slope Window")
+plt.ylabel("k1A Ratio")
+
+plt.subplot(2, 3, 6)
+plt.plot(slopeWindow,k1VExpectedRatio, 'g--', label = 'Expected Ratio')
+plt.plot(slopeWindow, k1VRatios, 'b', label = 'Actual Ratio')
+plt.legend()
+plt.title("The Effect of Slope Window on K1V Ratio")
+plt.xlabel("Slope Window")
+plt.ylabel("k1V Ratio")
 
 plt.show()
